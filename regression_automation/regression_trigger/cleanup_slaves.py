b8ba2a026a9594861eb902c3dc81bbf1312157a8
Raghav S K <raghav.sk@couchbase.com>
2025-11-18 10:03:09 +0530

    Improvements to cleanup_slaves script in regression_trigger
    1. Added a way to generate ini files of all slaves for next stage in pipeline(download builds)
    2. Added a way to filter slaves based on labels

--- regression_automation/regression_trigger/cleanup_slaves.py
+++ regression_automation/regression_trigger/cleanup_slaves.py
@@ -146,17 +146,24 @@ def clean_directories_on_slaves(slaves, username, password, days_threshold, keep
             future.result()
 
 def get_jenkins_slaves(jenkins_url):
-    url = f'{jenkins_url}computer/api/json?tree=computer[displayName]'
+    url = f"{jenkins_url}computer/api/json?tree=computer[displayName,assignedLabels[name]]"
     try:
         response = requests.get(url)
         response.raise_for_status()
         data = response.json()
-        slave_names = [node['displayName'] for node in data.get('computer', [])]
-        return slave_names
+        slaves = []
+        for node in data.get("computer", []):
+            display_name = node.get("displayName")
+            labels = [lbl.get("name") for lbl in node.get("assignedLabels", []) if "name" in lbl]
+            slaves.append({
+                "name": display_name,
+                "labels": labels
+            })
+        return slaves
     except requests.RequestException as e:
-        logger.error(f'HTTP error occurred: {e}')
+        logger.error(f"HTTP error occurred: {e}")
     except ValueError as e:
-        logger.error(f'JSON decode error: {e}')
+        logger.error(f"JSON decode error: {e}")
     return []
 
 def get_jenkins_slaves_ip(jenkins_url, count, slave_name, total, username, password, res_dic):
@@ -179,12 +186,18 @@ def get_jenkins_slaves_ip(jenkins_url, count, slave_name, total, username, passw
     except ET.ParseError as e:
         logger.error(f'XML parse error for {slave_name}: {e}')
 
-def get_all_slave_ips(jenkins_url, username, password):
+def get_all_slave_ips(jenkins_url, username, password, labels_filter=['P0']):
     slaves = get_jenkins_slaves(jenkins_url)
+    logger.info(f"Found {len(slaves)} slaves on Jenkins {jenkins_url}")
+    logger.info(f"Applying label filter: {labels_filter} and filtering slaves for cleanup")
+    filtered_slaves = [s['name'] for s in slaves]
+    if len(labels_filter) > 0:
+        filtered_slaves = [s['name'] for s in slaves if any(lbl in s['labels'] for lbl in labels_filter)]
+    logger.info(f"Found {len(filtered_slaves)} slaves on Jenkins {jenkins_url} after filtering")
     all_ips = {}
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=25) as executor:
-        futures = [executor.submit(get_jenkins_slaves_ip, jenkins_url, count+1, name, len(slaves), username, password, all_ips) for count, name in enumerate(slaves)]
+        futures = [executor.submit(get_jenkins_slaves_ip, jenkins_url, count+1, name, len(slaves), username, password, all_ips) for count, name in enumerate(filtered_slaves)]
         for future in concurrent.futures.as_completed(futures):
             future.result()
 
@@ -216,17 +229,45 @@ def get_connectable_ips(ip_list):
 
     return list(connectable_ips.values())
 
+def generate_ini(ip_list, output_file='slaves.ini'):
+    with open(output_file, 'w') as f:
+        f.write('[global]\n')
+        f.write(f'username:{SLAVE_USERNAME}\n')
+        f.write(f'password:{SLAVE_PASSWORD}\n\n')
+
+        f.write('[membase]\n')
+        f.write('rest_username:Administrator\n')
+        f.write('rest_password:password\n\n')
+
+        f.write('[servers]\n')
+        for i in range(1, len(ip_list) + 1):
+            f.write(f'{i}:_{i}\n')
+        f.write('\n')
+
+        for i, ip in enumerate(ip_list, 1):
+            f.write(f'[_{i}]\n')
+            f.write(f'ip:{ip}\n\n')
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Cleanup old build packages on Jenkins slave nodes.")
     parser.add_argument("--days_threshold", default=7, type=int, help="Days threshold for cleanup")
     parser.add_argument("--keep_recent", default=2, type=int, help="Number of recent builds to keep")
+    parser.add_argument('--ini_file_size', type=int, required=True, help='Number of slaves in ini file process')
+    parser.add_argument('--label_filter', type=str, default='', help='Comma separated labels to filter slaves from Jenkins')
 
     args = parser.parse_args()
+
+    label_filter = []
+    if args.label_filter:
+        label_filter = [lbl.strip() for lbl in args.label_filter.split(',') if lbl.strip()]
 
     all_ips = []
     for jenkins_url in JENKINS:
         logger.info(f'Fetching slave IPs from Jenkins {jenkins_url}')
-        all_ips = all_ips + get_all_slave_ips(jenkins_url, JENKINS[jenkins_url]["username"], JENKINS[jenkins_url]["password"])
+        all_ips = all_ips + get_all_slave_ips(jenkins_url, 
+                                              JENKINS[jenkins_url]["username"], 
+                                              JENKINS[jenkins_url]["password"], 
+                                              label_filter)
 
     logger.info(f'Checking connectivity to all slave IPs.')
     connectable_ips = get_connectable_ips(all_ips)
@@ -238,3 +279,8 @@ if __name__ == '__main__':
     clean_directories_on_slaves(connectable_ips, SLAVE_USERNAME, SLAVE_PASSWORD,
                                 days_threshold=args.days_threshold, keep_recent=args.keep_recent)
     logger.info('Cleanup process completed on all reachable slaves.')
+
+    logger.info('Generating ini files')
+    for i in range(0, len(connectable_ips), args.ini_file_size):
+        generate_ini(connectable_ips[i: i+args.ini_file_size], f'slaves_{i // args.ini_file_size}.ini')
+        logger.info(f'INI file generated: slaves_{i // args.ini_file_size}.ini')
